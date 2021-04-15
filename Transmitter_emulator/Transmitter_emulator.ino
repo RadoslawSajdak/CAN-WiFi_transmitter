d36fb62a696c7f1792da864df56e29d292536dc0
Radosaw Sajdak <sajdak.radoslaw@gmail.com>
2021-04-15 11:48:01 +0200

    Working emulator. Need to add ack verification

--- Transmitter_emulator/Transmitter_emulator.ino
+++ Transmitter_emulator/Transmitter_emulator.ino
@@ -2,14 +2,26 @@
 #include <CAN_config.h>
 
 CAN_device_t CAN_cfg;
+void emulateDevice(char * data, uint32_t address, uint8_t DLC)
+{
+  CAN_frame_t tx_frame;
 
+  tx_frame.FIR.B.FF = CAN_frame_std;
+  tx_frame.MsgID = address;
+  tx_frame.FIR.B.DLC = DLC;
+  for(uint8_t i = 0; i < DLC; i++)
+    tx_frame.data.u8[i] = data[i];
+  
+  ESP32Can.CANWriteFrame(&tx_frame);
+  delay(100); //We need to change it for sth like wait for ACK.
+}
 void setup() {
     Serial.begin(115200);
     Serial.println("iotsharing.com CAN demo");
     CAN_cfg.speed=CAN_SPEED_1000KBPS;
-    //CAN_cfg.tx_pin_id = GPIO_NUM_13;
+    //CAN_cfg.tx_pin_id = GPIO_NUM_13;  //ESP32-DevkitV1
     //CAN_cfg.rx_pin_id = GPIO_NUM_12;
-    CAN_cfg.tx_pin_id = GPIO_NUM_5;
+    CAN_cfg.tx_pin_id = GPIO_NUM_5; // ESP32-CAM
     CAN_cfg.rx_pin_id = GPIO_NUM_4;
     CAN_cfg.rx_queue = xQueueCreate(10,sizeof(CAN_frame_t));
     //start CAN Module
@@ -39,19 +51,15 @@ void loop() {
     }
     else
     {
-      rx_frame.FIR.B.FF = CAN_frame_std;
-      rx_frame.MsgID = 0x07;
-      rx_frame.FIR.B.DLC = 8;
-      rx_frame.data.u8[0] = 'h';
-      rx_frame.data.u8[1] = 'e';
-      rx_frame.data.u8[2] = 'l';
-      rx_frame.data.u8[3] = 'l';
-      rx_frame.data.u8[4] = 'o';
-      rx_frame.data.u8[5] = 'c';
-      rx_frame.data.u8[6] = 'a';
-      rx_frame.data.u8[7] = 'n';
-
       
-      ESP32Can.CANWriteFrame(&rx_frame);
+      emulateDevice("Device0 ", 0x1632, 8);
+      printf("Dev0 Done!\n\r");
+      emulateDevice("Device1 ", 0x011, 8);
+      printf("Dev1 Done!\n\r");
+      emulateDevice("Device2 ", 0x02, 8);
+      printf("Dev2 Done!\n\r");
+      emulateDevice("Device3 ", 0x11fc, 8);
+      printf("Dev3 Done!\n\r");
+      
     }
 }
